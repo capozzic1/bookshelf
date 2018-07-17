3529d624c583c0cd3ac6439de5a8ab825fc1af26
Chris <capozzic1@gmail.com>
2018-07-17 09:48:55 -0500

    Initial commit

--- src/app/shared/book.service.ts
+++ src/app/shared/book.service.ts
@@ -1,11 +1,16 @@
+import { Book } from './book';
 import { Injectable, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+
 
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class BookService {
 
   constructor(private http: HttpClient) { }
@@ -13,9 +18,18 @@ export class BookService {
   getBooks(query: string): Observable<any> {
     const API_KEY = 'AIzaSyCYGuQ37yG2Bl0HIXVl-jRbjQYrsIuGDGE';
     const queryURL = `https://www.googleapis.com/books/v1/volumes?q=${query}&key=${API_KEY}`;
-    
-    return this.http.get(queryURL);
+    return this.http.get(queryURL).pipe(
+      map((data: any) => {
+        data.items.map(item => {
+            return this.bookFactory(item);
+        });
+      })
+    )
   }
 
+  bookFactory(item: any): Book {
+   // console.log(item)
+    return new Book(item.volumeInfo.title);
+  }
 
 }
