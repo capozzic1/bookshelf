d720a916a1c182b02f90d6492b560d48f1115722
Chris <capozzic1@gmail.com>
2018-08-01 10:46:27 -0500

    Updated styling

--- src/app/pipes/truncate.pipe.ts
+++ src/app/pipes/truncate.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class TruncatePipe implements PipeTransform {
     transform(value: string) {
-        const limit = value.substr(0, 50).lastIndexOf(' ');
+        const limit = value.substr(0, 30).lastIndexOf(' ');
         const ellipsis = '...';
         return `${value.substr(0, limit)}${ellipsis}`;
 
